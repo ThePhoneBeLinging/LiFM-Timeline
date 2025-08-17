41b1e0ecc51e50bb9b27b6602c5380af233ff972
EAL <elias@takefive.dk>
2025-08-17 10:41:00 +0200

    now compiles

--- test/TestTemplate.cpp
+++ test/TestTemplate.cpp
@@ -2,6 +2,8 @@
 // Created by eal on 29/07-2025.
 //
 #include "gtest/gtest.h"
+#include "LiFM-Timeline/LiFMTimeline.hpp"
+
 class TestTemplate : public testing::Test
 {
 
@@ -9,5 +11,6 @@ class TestTemplate : public testing::Test
 
 TEST_F(TestTemplate, Constructor)
 {
+ auto thing = std::make_shared<LiFMTimeline>(std::chrono::system_clock::now());
  EXPECT_TRUE(true);
 }
